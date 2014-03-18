a6df9350ff45de5f0c10a566566c2021faa6ce5a
CoderPuppy <cpup@cpup-laptop>
2014-03-18 14:20:14 -0400

    Use the new version of checkNull

--- src/main/scala/cpup/mc/magic/content/ItemSpell.scala
+++ src/main/scala/cpup/mc/magic/content/ItemSpell.scala
@@ -5,16 +5,13 @@ import net.minecraft.entity.player.EntityPlayer
 import java.util
 import cpup.mc.magic.api.impl.{InvalidTransformException, ContextFactory, Parser}
 import cpup.lib.Util
-import cpup.mc.magic.api.TRune
-import net.minecraft.nbt.NBTTagCompound
+import cpup.mc.lib.util.ItemUtil
 
 class ItemSpell extends ItemBase {
 	override def addInformation(stack: ItemStack, player: EntityPlayer, _lore: util.List[_], par4: Boolean) {
 		val lore = _lore.asInstanceOf[util.List[String]]
 
-		val spell = Util.checkNull(stack.getTagCompound, (compound: NBTTagCompound) => {
-			Util.checkNull(compound.getString("spell"), (str: String) => str, "")
-		}, "")
+		val spell = Util.checkNull(ItemUtil.compound(stack).getString("spell"), "")
 
 		lore.add(spell)
 
