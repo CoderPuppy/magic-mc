da01183ecd3046b491f4e4445bb73810b52b5da6
CoderPuppy <cpup@cpup-laptop>
2014-03-18 23:35:00 -0400

    Break both parts of the writing desk when one is broken

--- src/main/scala/cpup/mc/magic/content/BlockWritingDesk.scala
+++ src/main/scala/cpup/mc/magic/content/BlockWritingDesk.scala
@@ -16,17 +16,38 @@ class BlockWritingDesk extends Block(Material.wood) with TBlockBase with CPupBlo
 	setHardness(1)
 	setResistance(2.5f)
 
+	override def breakBlock(world: World, x: Int, y: Int, z: Int, block: Block, meta: Int): Unit = {
+		super.breakBlock(world, x, y, z, block, meta)
+
+		val pos = BlockPos(world, x, y, z)
+		val dir = Direction.fromFacing(meta >> 1)
+		val isSlave = (meta & 1) == 0
+
+		if(isSlave) {
+			val masterPos = pos.offset(dir.rotated(Direction.Up))
+			if(masterPos.block == this) {
+				masterPos.setBlock(Blocks.air)
+			}
+		} else {
+			val slavePos = pos.offset(dir.unrotated(Direction.Up))
+			if(slavePos.block == this) {
+				slavePos.setBlock(Blocks.air)
+			}
+		}
+	}
+
 	override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, placer: EntityLivingBase, stack: ItemStack) {
 		val dir = Direction.fromYaw(placer.rotationYaw).opposite
-		val thisPos = BlockPos(world, x, y, z)
+
+		val pos = BlockPos(world, x, y, z)
 			.setMetadata((dir.facing << 1) | 1, 2)
-		val slavePos = thisPos.offset(dir.unrotated(Direction.Up))
+		val slavePos = pos.offset(dir.unrotated(Direction.Up))
 		if(slavePos.isReplaceable) {
 			slavePos
 				.setBlock(this)
 				.setMetadata(dir.facing << 1, 2)
 		} else {
-			thisPos.setBlock(Blocks.air)
+			pos.setBlock(Blocks.air)
 			if(placer.isInstanceOf[EntityPlayer]) {
 				val player = placer.asInstanceOf[EntityPlayer]
 				player.inventory.addItemStackToInventory(stack)
