2a797996f51fca99530dd9bc273fe1134a22d165
CoderPuppy <cpup@cpup-laptop>
2014-03-18 22:45:44 -0400

    Add the start for multiblock writing table

--- src/main/scala/cpup/mc/magic/content/BlockWritingDesk.scala
+++ src/main/scala/cpup/mc/magic/content/BlockWritingDesk.scala
@@ -9,13 +9,31 @@ import net.minecraft.item.ItemStack
 import cpup.mc.lib.util.Direction
 import cpup.mc.lib.util.pos.BlockPos
 import net.minecraft.block.material.Material
+import net.minecraft.init.Blocks
+import net.minecraft.entity.player.EntityPlayer
 
 class BlockWritingDesk extends Block(Material.wood) with TBlockBase with CPupBlockContainer[TMagicMod] {
 	setHardness(1)
 	setResistance(2.5f)
 
 	override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, placer: EntityLivingBase, stack: ItemStack) {
-		val dir = Direction.fromYaw(placer.rotationYaw)
-		BlockPos(world, x, y, z).setMetadata(dir.opposite.facing, 2)
+		val dir = Direction.fromYaw(placer.rotationYaw).opposite
+		val thisPos = BlockPos(world, x, y, z)
+			.setMetadata((dir.facing << 1) | 1, 2)
+		val slavePos = thisPos.offset(dir.unrotated(Direction.Up))
+		if(slavePos.block == Blocks.air) {
+			slavePos
+				.setBlock(this)
+				.setMetadata(dir.facing << 1, 2)
+		} else {
+			thisPos.setBlock(Blocks.air)
+			if(placer.isInstanceOf[EntityPlayer]) {
+				val player = placer.asInstanceOf[EntityPlayer]
+				player.inventory.addItemStackToInventory(stack)
+			} else {
+				// This might be buggy...
+				placer.entityDropItem(stack, 0.4)
+			}
+		}
 	}
 }
