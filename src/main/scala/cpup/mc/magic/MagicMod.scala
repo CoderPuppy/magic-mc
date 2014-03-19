df4bcdd2635b795f5d0393e6bbd34c5616d6298b
CoderPuppy <cpup@cpup-laptop>
2014-03-19 12:37:01 -0400

    Update for networking stuff

--- src/main/scala/cpup/mc/magic/MagicMod.scala
+++ src/main/scala/cpup/mc/magic/MagicMod.scala
@@ -3,8 +3,9 @@ package cpup.mc.magic
 import cpup.mc.lib.CPupMod
 import cpw.mods.fml.common.Mod
 import cpup.mc.magic.content.Content
+import cpup.mc.lib.network.CPupMessage
 
-trait TMagicMod extends CPupMod[TRef] {
+trait TMagicMod extends CPupMod[TRef, CPupMessage] {
 	def ref = Ref
 	override def content = Content
 }
