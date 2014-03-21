0626e8a8d6c03131142cf1776df0349ad3e313c0
CoderPuppy <cpup@cpup-laptop>
2014-03-20 22:33:01 -0400

    Add entities

--- src/main/scala/cpup/mc/magic/client/runeSelection/RootCategory.scala
+++ src/main/scala/cpup/mc/magic/client/runeSelection/RootCategory.scala
@@ -1,10 +1,22 @@
 package cpup.mc.magic.client.runeSelection
 
+import net.minecraft.entity.EntityList
+
 object RootCategory {
 	def create = {
 		val root = new Category("root")
 
-		root.addOption(RuneOption("p!of"))
+		val entities = new Category("entities")
+
+		for(name <- EntityList.stringToClassMapping.keySet.toArray) {
+			entities.addOption(RuneOption("tn!entity!" + name))
+		}
+
+		root.addOption(entities)
+
+		val stuff = new Category("stuff")
+		stuff.addOption(RuneOption("p!of"))
+		root.addOption(stuff)
 
 		root
 	}
