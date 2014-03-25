7fe1347fdb13277b0d64a9b175eb3798667a6b0a
CoderPuppy <cpup@cpup-laptop>
2014-03-25 15:29:27 -0400

    Growing and debugging

--- src/main/scala/cpup/mc/magic/api/oldenLanguage/casters/PlayerCaster.scala
+++ src/main/scala/cpup/mc/magic/api/oldenLanguage/casters/PlayerCaster.scala
@@ -7,7 +7,6 @@ import cpw.mods.fml.common.FMLCommonHandler
 
 class PlayerCaster(val player: EntityPlayer) extends TCaster {
 	def world = player.worldObj
-	println(FMLCommonHandler.instance.getEffectiveSide)
 	val mop = EntityUtil.getMOPBoth(player, if(player.capabilities.isCreativeMode) 6 else 3)
 
 	def entities = List(player)
--- src/main/scala/cpup/mc/magic/content/runes/GrowRune.scala
+++ src/main/scala/cpup/mc/magic/content/runes/GrowRune.scala
@@ -6,13 +6,14 @@ import net.minecraft.util.IIcon
 import cpup.mc.magic.MagicMod
 import cpup.mc.lib.util.pos.BlockPos
 import net.minecraft.entity.{EntityAgeable, Entity}
+import cpw.mods.fml.common.FMLCommonHandler
 
 object GrowRune extends SingletonRune with TAction {
 	def mod = MagicMod
 
 	def actUponBlock(pos: BlockPos) {
 		for(_ <- 0 to 20) {
-			pos.scheduleUpdateTick(5)
+			pos.scheduleUpdateTick(0)
 		}
 	}
 
@@ -20,6 +21,7 @@ object GrowRune extends SingletonRune with TAction {
 		entity match {
 			case entity: EntityAgeable =>
 				entity.addGrowth(20000)
+			case _ =>
 		}
 	}
 
