a6fdc4259c6bd5eb477cfeda7dfde72364f7bf93
CoderPuppy <cpup@cpup-laptop>
2014-03-26 14:45:27 -0400

    Rendering for bending

--- src/main/scala/cpup/mc/magic/client/ClientEvents.scala
+++ src/main/scala/cpup/mc/magic/client/ClientEvents.scala
@@ -7,14 +7,21 @@ import net.minecraft.client.settings.KeyBinding
 import cpup.mc.magic.MagicMod
 import cpw.mods.fml.common.gameevent.TickEvent.Phase
 import net.minecraft.client.Minecraft
-import net.minecraftforge.client.event.RenderGameOverlayEvent
+import net.minecraftforge.client.event.{RenderWorldLastEvent, RenderGameOverlayEvent}
 import scala.collection.mutable
+import net.minecraft.client.renderer.{RenderBlocks, Tessellator}
+import org.lwjgl.opengl.GL11
+import cpup.mc.magic.content.ItemBend
+import net.minecraft.util.ResourceLocation
+import net.minecraft.init.Blocks
 
 class ClientEvents(val proxy: ClientProxy) {
 	val mc = Minecraft.getMinecraft
+	val renderBlocks = new RenderBlocks
 
 	def mod = MagicMod
 
+	// -- Wand spell casting --
 	val keysDown = new mutable.HashMap[KeyBinding, Boolean]()
 	var firstEvent = true
 	val allowedKeys = List(
@@ -27,7 +34,7 @@ class ClientEvents(val proxy: ClientProxy) {
 	)
 
 	@SubscribeEvent
-	def handleKeyboardInput(e: InputEvent.KeyInputEvent) {
+	def castingKeys(e: InputEvent.KeyInputEvent) {
 		if(proxy.selector != null && mc.theWorld != null) {
 			val key = Keyboard.getEventKey
 
@@ -64,7 +71,7 @@ class ClientEvents(val proxy: ClientProxy) {
 	}
 
 	@SubscribeEvent
-	def resetKeys(e: TickEvent.ClientTickEvent) {
+	def resetCastingKeys(e: TickEvent.ClientTickEvent) {
 		if(e.phase == Phase.END && mc.theWorld != null) {
 			firstEvent = true
 			keysDown.clear
@@ -72,9 +79,51 @@ class ClientEvents(val proxy: ClientProxy) {
 	}
 
 	@SubscribeEvent
-	def renderOptions(e: RenderGameOverlayEvent) {
+	def renderCastingOptions(e: RenderGameOverlayEvent) {
 		if(proxy.selector != null && mc.theWorld != null && !e.isCancelable && e.`type` == RenderGameOverlayEvent.ElementType.HOTBAR) {
 			proxy.selector.render
+		}
+	}
+
+	// -- Bend --
+	val bendTexture = new ResourceLocation(mod.ref.modID, "textures/misc/bend.png")
+
+	@SubscribeEvent
+	def renderBend(e: RenderWorldLastEvent) {
+		val tess = Tessellator.instance
+		val renderEntity = mc.renderViewEntity
+		val player = mc.thePlayer
+		val textureManager = mc.getTextureManager
+
+		if(player.isUsingItem && player.getItemInUse.getItem == mod.content.items("bend")) {
+			val pos = mod.content.items("bend").asInstanceOf[ItemBend].getFarLook(player.getItemInUse, player, player.getItemInUseCount)
+
+			GL11.glPushMatrix
+			GL11.glTranslated(
+				-(renderEntity.lastTickPosX + (renderEntity.posX - renderEntity.lastTickPosX) * e.partialTicks),
+				-(renderEntity.lastTickPosY + (renderEntity.posY - renderEntity.lastTickPosY) * e.partialTicks),
+				-(renderEntity.lastTickPosZ + (renderEntity.posZ - renderEntity.lastTickPosZ) * e.partialTicks)
+			)
+			GL11.glTranslated(pos.xCoord + 0.5, pos.yCoord, pos.zCoord + 0.5)
+			GL11.glDisable(GL11.GL_LIGHTING)
+
+			renderBlocks.setRenderBounds(0.05, 0.05, 0.05, 0.95, 0.95, 0.95)
+			tess.startDrawingQuads
+			tess.setColorRGBA(255, 255, 255, 100)
+			tess.setBrightness(200)
+			textureManager.bindTexture(bendTexture)
+			val block = Blocks.stone
+			val icon = block.getBlockTextureFromSide(0)
+			renderBlocks.renderFaceXNeg(block, -0.5, 0, -0.5, icon)
+			renderBlocks.renderFaceXPos(block, -0.5, 0, -0.5, icon)
+			renderBlocks.renderFaceYNeg(block, -0.5, 0, -0.5, icon)
+			renderBlocks.renderFaceYPos(block, -0.5, 0, -0.5, icon)
+			renderBlocks.renderFaceZNeg(block, -0.5, 0, -0.5, icon)
+			renderBlocks.renderFaceZPos(block, -0.5, 0, -0.5, icon)
+			tess.draw
+
+			GL11.glEnable(GL11.GL_LIGHTING)
+			GL11.glPopMatrix
 		}
 	}
 }
