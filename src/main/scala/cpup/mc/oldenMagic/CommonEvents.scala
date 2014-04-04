5091d0b4a6c46930fb2cdef290ac280f8c43f2af
CoderPuppy <cpup@cpup-laptop>
2014-04-04 01:23:09 -0400

    Work on passive spells

--- src/main/scala/cpup/mc/oldenMagic/CommonEvents.scala
+++ src/main/scala/cpup/mc/oldenMagic/CommonEvents.scala
@@ -3,6 +3,9 @@ package cpup.mc.oldenMagic
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.event.entity.player.PlayerInteractEvent
 import net.minecraft.item.ItemStack
+import net.minecraftforge.event.entity.living.LivingHurtEvent
+import cpup.mc.lib.util.WorldSavedDataUtil
+import cpup.mc.oldenMagic.api.oldenLanguage.PassiveSpellsData
 
 class CommonEvents {
 	def mod = MagicMod
@@ -12,5 +15,29 @@ class CommonEvents {
 //		if(e.entityPlayer.inventory.getCurrentItem == null && (e.action == PlayerInteractEvent.Action.RIGHT_CLICK_AIR || e.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK)) {
 //			e.entityPlayer.inventory.setInventorySlotContents(e.entityPlayer.inventory.currentItem, new ItemStack(mod.content.items("bend")))
 //		}
+	}
+
+	@SubscribeEvent
+	def passiveDamage(e: LivingHurtEvent) {
+		var passiveSpellDatas = WorldSavedDataUtil.get(e.entity.worldObj, classOf[PassiveSpellsData], "passive(ender)")
+
+		val baseX = e.entity.chunkCoordX
+		val baseZ = e.entity.chunkCoordZ
+
+		for {
+			offsetX <- -1 to 1
+			offsetZ <- -1 to 1
+		} {
+			val x = baseX + offsetX
+			val z = baseZ + offsetZ
+
+			println(baseX, baseZ)
+			println(offsetX, offsetZ)
+			println(x, z)
+
+			passiveSpellDatas ++= List(WorldSavedDataUtil.get(e.entity.worldObj, classOf[PassiveSpellsData], s"$x,$z"))
+		}
+
+		println(passiveSpellDatas)
 	}
 }
