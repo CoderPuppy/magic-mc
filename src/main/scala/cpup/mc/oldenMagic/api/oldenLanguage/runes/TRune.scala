51b17d5bde047c422d97bd524365d8ff02c75db7
CoderPuppy <cpup@cpup-laptop>
2014-05-12 12:00:14 -0400

    Add SideOnly to TRune.icons

--- src/main/scala/cpup/mc/oldenMagic/api/oldenLanguage/runes/TRune.scala
+++ src/main/scala/cpup/mc/oldenMagic/api/oldenLanguage/runes/TRune.scala
@@ -3,9 +3,12 @@ package cpup.mc.oldenMagic.api.oldenLanguage.runes
 import net.minecraft.util.IIcon
 import cpup.mc.lib.util.GUIUtil
 import net.minecraft.nbt.NBTTagCompound
+import cpw.mods.fml.relauncher.{Side, SideOnly}
 
 trait TRune {
 	def runeType: TRuneType
+
+	@SideOnly(Side.CLIENT)
 	def icons: List[IIcon]
 
 	def render(x: Int, y: Int, width: Int, height: Int) {
