d0e4706ee9de9af044ec77d7ace3788457e4cc1b
CoderPuppy <cpup@cpup-laptop>
2014-05-12 22:30:07 -0400

    Fix the CastingRegistry

--- src/main/scala/cpup/mc/oldenMagic/api/oldenLanguage/casting/CastingRegistry.scala
+++ src/main/scala/cpup/mc/oldenMagic/api/oldenLanguage/casting/CastingRegistry.scala
@@ -6,13 +6,13 @@ import cpup.mc.lib.targeting.TTarget
 object CastingRegistry {
 	private val wrappers = new mutable.HashMap[Class[TTarget], TTarget => TCaster]
 
-	def register[T <: TTarget](cla: Class[T], wrapper: T => TCaster) {
+	def registerCaster[T <: TTarget](cla: Class[T], wrapper: T => TCaster) {
 		wrappers(cla.asInstanceOf[Class[TTarget]]) = wrapper.asInstanceOf[TTarget => TCaster]
 	}
 
 	def wrap(target: TTarget): Option[TCaster] = {
 		var cla = target.getClass.asInstanceOf[Class[TTarget]]
-		while(cla.getInterfaces.contains(classOf[TTarget])) {
+		while(classOf[TTarget].isAssignableFrom(cla)) {
 			if(wrappers.contains(cla)) {
 				return Some(wrappers(cla)(target))
 			} else if(cla.getSuperclass == null) {
